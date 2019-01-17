0994b5adaccfbb25deccb38c09faf59fe7ff0841
TMatome <46716771+TMatome@users.noreply.github.com>
2019-01-17 11:23:52 +0200

    circles files

---
+++ GitWorkshopCircle/GitWorkshop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float radius = 0;
+           Console.WriteLine("Please enter the radius: ");
+           radius =  float.Parse(Console.ReadLine());
+
+            var circle = new Circle(radius);
+            Console.WriteLine("The Area of a Circle with radius {0} is {1}",radius,circle.Area);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+    }
+}
